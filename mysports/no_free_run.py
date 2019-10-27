4ba8acbce298e62039a05724bace5e1e51ba6ddf
Hexwell <pocoa@foxmail.com>
2019-10-27 19:46:58 +0800

    解决冲突

--- mysports/no_free_run.py
+++ mysports/no_free_run.py
@@ -11,6 +11,9 @@ from path_plan.plan import path_plan, get_school_location
 def no_free_run(userid: str, ses, extra_pn=1, school="", rg=(2, 4), debug=False):
     data = json.dumps({"initLocation": "121.889662,30.895456", "type": "1", "userid": userid})
     res = ses.get(host + '/api/run/runPage', params={'sign': get_md5_code(data), 'data': data.encode('ascii')})
+    if res.json()['code'] == 404:
+        print('<NoFreeRunModule>: 体育锻炼接口返回 JSON：',res.json()['msg'])
+        return
     resj = res.json()['data']
     print('<NoFreeRunModule>: 体育锻炼接口返回 JSON', resj)
 
