ffba26d7d48f1180edb0c625bc8c6f32fd056108
Hexwell <pocoa@foxmail.com>
2020-11-06 13:54:03 +0800

    chore(util): add GetSchoolLocation func

---
+++ model/location.go
@@ -0,0 +1,7 @@
+package model
+
+// 学校地址
+type SchoolLocationStruct struct {
+	Lng float64 `json:"lng"`
+	Lat float64 `json:"lat"`
+}
--- util/baidu_map.go
+++ util/baidu_map.go
@@ -1,24 +1,32 @@
 package util
 
 import (
-	"fmt"
+	"errors"
 	"fsc/global"
+	"fsc/model"
 	"github.com/menduo/gobaidumap"
 )
 
-func GetSchoolLocation(school string) (err error) {
-	mp := make(map[string]string, 0)
-	mp["address"] = school
-	mp["output"] = "json"
-
+func GetSchoolLocation(school string) (loc model.SchoolLocationStruct, err error) {
+	var schoolLocation model.SchoolLocationStruct
+	// 百度 AK 判空
+	if global.FSC_CONFIG.BaiduMapConfig.AccessKey == "" {
+		return schoolLocation, errors.New("请先配置百度 AccessKey ")
+	}
+	// 百度 SDK
 	bc := gobaidumap.NewBaiduMapClient(global.FSC_CONFIG.BaiduMapConfig.AccessKey)
+	// 地址转经纬
 	addressToGEO, err := bc.GetGeoViaAddress(school)
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("从地址到坐标 - All", addressToGEO)
-		fmt.Println("从地址到坐标 - Lat", addressToGEO.Result.Location.Lat)
-		fmt.Println("从地址到坐标 - Lng", addressToGEO.Result.Location.Lng)
+		return schoolLocation, errors.New("获取学校地址失败")
 	}
-	return err
+	if global.FSC_CONFIG.FSC.Debug {
+		global.FSC_LOG.Info("学校地址 - All", addressToGEO)
+		global.FSC_LOG.Info("学校地址 - Lat", addressToGEO.Result.Location.Lat)
+		global.FSC_LOG.Info("学校地址 - Lng", addressToGEO.Result.Location.Lng)
+	}
+	schoolLocation.Lat = addressToGEO.Result.Location.Lat
+	schoolLocation.Lng = addressToGEO.Result.Location.Lng
+	return schoolLocation, nil
+
 }
